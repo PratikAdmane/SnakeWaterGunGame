8cd38b7752bc1e78f75f6670acd6e3b89b3e61ff
Pratik Admane <165630278+PratikAdmane@users.noreply.github.com>
2024-04-24 20:53:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let user= prompt("Enter s w or g ")
+let cpuI=Math.floor(Math.random() * 3);
+let cpu=["s", "w", "g"][cpuI]
+
+const match=(user, cpu)=>{
+    if(user===cpu){
+        return "Tie"
+    }
+    else if(user==="s" && cpu==="w"){
+        return "user"
+    }
+    else if(user==="s" && cpu==="g"){
+        return "cpu"
+    }
+    else if(user==="w" && cpu==="s"){
+        return "cpu"
+    }
+    else if(user==="w" && cpu==="g"){
+        return "user"
+    }
+    else if(user==="g" && cpu==="s"){
+        return "user"
+    }
+    else if(user==="g" && cpu==="w"){
+        return "cpu"
+    }
+}
+let result=match(user, cpu)
+//console.log("user:${user}<br> cpu:${cpu} <br> The winner is: ${result.toUpperCase}")
+document.write(`<br> user:${user}<br> cpu:${cpu} <br> The winner is: ${result.toUpperCase()}`)
+
+
